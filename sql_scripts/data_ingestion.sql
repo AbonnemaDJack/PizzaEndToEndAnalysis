964e63c38f88e1cdb32b1292e3236095386f033b
AbonnemaDJack <tonyelawsonjack3@gmail.com>
2025-05-06 16:10:13 +0100

    analysis in sql

--- sql_scripts/data_ingestion.sql
+++ sql_scripts/data_ingestion.sql
@@ -37,3 +37,28 @@ left join order_details as od
 on od.pizza_id = p.pizza_id
 GROUP BY pt.name, p.pizza_id, p.price, od.quantity
 order by revenue desc limit 1;
+
+-- get the total revdenue of the most ordered pizza
+SELECT pt.name, SUM(od.quantity * p.price) as revenue
+from pizza_types as pt 
+left join pizzas as p                
+on p.pizza_type_id = pt.pizza_type_id
+left join order_details as od   
+on od.pizza_id = p.pizza_id
+GROUP BY pt.name
+order by revenue desc limit 1;
+
+-- which pizza contributed the most revenue each year from 2023 to 2025
+SELECT
+    pt.name,
+    EXTRACT(YEAR FROM order_date::DATE) as YEAR,
+    sum(od.quantity * p.price) as total_revenue
+from pizza_types as pt
+left join pizzas as p                
+on p.pizza_type_id = pt.pizza_type_id
+left join order_details as od   
+on od.pizza_id = p.pizza_id
+left join orders as o
+on o.order_id = od.order_id
+GROUP BY year, name 
+order by year, total_revenue desc;
