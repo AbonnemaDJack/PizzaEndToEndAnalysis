f32972f4269558c51ab7841f977f6bce1f99704c
AbonnemaDJack <tonyelawsonjack3@gmail.com>
2025-05-06 15:09:46 +0100

    Data Analysis with SQL

--- sql_scripts/data_ingestion.sql
+++ sql_scripts/data_ingestion.sql
@@ -18,3 +18,22 @@ create TABLE order_details as (
  SELECT * FROM order_details_2025
 );
 SELECT * FROM order_details;
+select  *from orders
+
+select count(order_id)
+from orders
+GROUP BY order_id
+HAVING COUNT(order_id) > 1;
+
+select * from pizzas;
+
+-- The most ordered pizza type
+
+SELECT pt.name, p.pizza_id,(p.price * od.quantity) as revenue
+from pizza_types as pt 
+left join pizzas as p                
+on p.pizza_type_id = pt.pizza_type_id
+left join order_details as od   
+on od.pizza_id = p.pizza_id
+GROUP BY pt.name, p.pizza_id, p.price, od.quantity
+order by revenue desc limit 1;
